e59f576de4332ce114357df3355f0d65173c2574
Thibaud Basset <basset.thibaud@gmail.com>
2022-08-01 14:39:28 +0200

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html.twig',
+    './assets/js/**/*.js',
+    './src/**/*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
