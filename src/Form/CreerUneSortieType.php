be8622226540edef3161503e40ebc5f0c980d114
mattMAY <107032969+MrDezem@users.noreply.github.com>
2022-08-04 10:50:11 +0200

    Update CreerUneSortieType.php
    Todo longitude et latitude

--- src/Form/CreerUneSortieType.php
+++ src/Form/CreerUneSortieType.php
@@ -4,25 +4,46 @@ namespace App\Form;
 
 use App\Entity\Sortie;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Time;
 
 class CreerUneSortieType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('dateHeureDebut')
-            ->add('duree')
-            ->add('dateLimiteInscription')
+            ->add('nom', TextType::class)
+            ->add('dateHeureDebut', DateTimeType::class, [
+                'html5' => true,
+            ])
+            ->add('duree', Time::class)
+            ->add('dateLimiteInscription', DateTimeType::class, [
+                'html5' => true,
+            ])
             ->add('nbInscriptionsMax')
-            ->add('infosSortie')
+            ->add('infosSortie', TextareaType::class)
             ->add('campus')
-            ->add('etat')
+            ->add('etat', ChoiceType::class, [
+                'choices' => [
+                    'Créée' => 'Créée',
+                    'Ouverte' => 'Ouverte',
+                    'Clôturée' => 'Clôturée',
+                    'Activité en cours' => 'Activité en cours',
+                    'Passée' => 'Passée',
+                    'Annulée' => 'Annulée'
+                ]
+            ])
             ->add('lieu')
             ->add('usersInscrits')
             ->add('userOrganisateur')
+            ->add('nom', VilleAddType::class)
+            ->add('code_postal', VilleAddType::class)
+            //Todo longitude et latitude
         ;
     }
 
