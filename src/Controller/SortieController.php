28a85d6cdc66a51fd74b59a27859ad2145f01200
Nerz <adrian.dufac@gmail.com>
2022-08-08 16:36:09 +0200

    ajout sorties
    créationd de la page d'ajout de sortie

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\Sortie;
 use App\Form\CreerUneSortieType;
+use App\Repository\EtatRepository;
 use App\Repository\SortieRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -17,18 +20,21 @@ class SortieController extends AbstractController
     /**
      * @Route("/créer", name="sortie_créer")
      */
-    public function créer(EntityManagerInterface $entityManager){
+    public function créer(EntityManagerInterface $entityManager, Request $request,EtatRepository  $etatRepository,UserRepository $userRepository){
         $sortie =new Sortie();
         $sortieForm = $this->createForm(CreerUneSortieType::class,$sortie);
+        $sortieForm->handleRequest($request);
 
         if($sortieForm->isSubmitted()){
+
+            $sortie->setEtat($etatRepository->findOneBy(['libelle'=>'Créée']));
+            $sortie->setUserOrganisateur($userRepository->findOneBy(['email'=>$this->getUser()->getUserIdentifier()]));
             $entityManager->persist($sortie);
             $entityManager->flush();
             // redirect page liste des sorties
-            $this->redirectToRoute('gestion_ville');
+            return $this->redirectToRoute('main_page');
         }
         return $this->render('sortie/ajoutersortie.html.twig',['sortieForm' =>$sortieForm->createView()]);
-
     }
 
     /**
