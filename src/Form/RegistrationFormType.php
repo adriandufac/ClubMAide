8971d9363c5d2d4d2311eb6145fd7ec308952c9d
REDANE BADAOUI <103901647+RedaneDev@users.noreply.github.com>
2022-08-11 11:45:06 +0200

    correction bug
    - correction new user

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -2,16 +2,17 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use App\Entity\Campus;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class RegistrationFormType extends AbstractType
